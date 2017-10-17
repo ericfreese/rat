1ca25ec45817ffc7f5c7c0dd8adc1d5448eb763b
Eric Freese <ericdfreese@gmail.com>
2017-10-17 17:24:41 -0600

    Add simple version check
    Run `rat --version` to output version information.

--- main.go
+++ main.go
@@ -2,26 +2,38 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	rat "github.com/ericfreese/rat/lib"
 )
 
+var (
+	RAT_VERSION = "0.0.2"
+)
+
 var flags struct {
-	cmd  string
-	mode string
+	cmd     string
+	mode    string
+	version bool
 }
 
 func init() {
 	flag.StringVar(&flags.cmd, "cmd", "cat ~/.config/rat/ratrc", "command to run")
 	flag.StringVar(&flags.mode, "mode", "default", "name of mode")
+	flag.BoolVar(&flags.version, "version", false, "display version and exit")
 
 	flag.Parse()
 }
 
 func main() {
 	var err error
+
+	if flags.version {
+		fmt.Println(RAT_VERSION)
+		return
+	}
 
 	if err = rat.Init(); err != nil {
 		panic(err)
